39a14f7e98dcd34611b50d71a42f6f6e6ab447f1
michaelryancaputo <me@mikecaputo.com>
2015-02-11 13:59:41 -0500

    Add in 'Good'

--- app/templates/_server.js
+++ app/templates/_server.js
@@ -7,6 +7,29 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var Hapi = require('hapi');
 var config = require('./lib/config/config');
 var server = new Hapi.Server();
+var good = require('good');
+var goodConsole = require('good-console');
+var goodFile = require('good-file');
+var goodHttp = require('good-http');
+
+var goodConfig = {
+    opsInterval: 1000,
+    reporters: [{
+        reporter: goodConsole,
+        args:[{ log: '*', response: '*' }]
+    }, {
+        reporter: goodFile,
+        args: ['./test/fixtures/awesome_log', { ops: '*' }]
+    }, {
+        reporter: goodHttp,
+        args: [{ error: '*' }, 'http://prod.logs:3000', {
+            threshold: 20,
+            wreck: {
+                headers: { 'x-api-key' : 12345 }
+            }
+        }]
+    }]
+};
 
 /**
  * Creating Server connection with our configuration
@@ -17,20 +40,27 @@ server.connection(config.server);
  * Adding routes
  */
 server.route(require('./lib/controllers/routes'));
-//var routes = require('./lib/controllers/routes')(server);
 
 /**
  * Adding plugins
  */
 server.register({register: require('lout')}, function(err) {if(err) {console.log(err); }});
+server.register({
+  register: good,
+  options: options
+}, function (err) {
+  if (err) {console.error(err);}
 
-/**
- * Starting server
- */
-if (!module.parent) {
-  server.start(function () {
-    console.log('Server running at:', server.info.uri);
-  });
-}
+  else {
+    /**
+     * Starting server
+     */
+    if (!module.parent) {
+      server.start(function () {
+        console.info('Server started at ' + server.info.uri);
+      });
+    }
+  }
+});
 
 module.exports = server;
