8a9c1fcf96dc661e1dc5282092d0310dcc6dd858
michaelryancaputo <me@mikecaputo.com>
2015-02-12 10:51:28 -0500

    Add tests for controller

--- controller/templates/test.js
+++ controller/templates/test.js
@@ -5,20 +5,50 @@ var Lab = require('lab');
 var server = require('../server.js');
 var lab = exports.lab = Lab.script();
 
-lab.experiment('Hello Tests', function() {
+lab.experiment('<%= humanizedSingularName %> Tests', function() {
 
-  lab.test('main endpoint says \'Hello World!\'', function(done) {
+  lab.test('<%= humanizedSingularName %> endpoint POST req', function(done) {
     var options = {
-      method: 'GET',
-      url: '/'
+      method: 'POST',
+      url: '/<%= humanizedSingularName %>'
     };
-
     server.inject(options, function(res) {
       code.expect(res.statusCode).to.equal(200);
-      code.expect(res.result).to.equal('Hello world!');
       done();
     });
   });
 
+  lab.test('<%= humanizedSingularName %> endpoint GET req', function(done) {
+    var options = {
+      method: 'GET',
+      url: '/<%= humanizedSingularName %>'
+    };
+    server.inject(options, function(res) {
+      code.expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  lab.test('<%= humanizedSingularName %> endpoint PUT req', function(done) {
+    var options = {
+      method: 'PUT',
+      url: '/<%= humanizedSingularName %>'
+    };
+    server.inject(options, function(res) {
+      code.expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  lab.test('<%= humanizedSingularName %> endpoint DELETE req', function(done) {
+    var options = {
+      method: 'DELETE',
+      url: '/<%= humanizedSingularName %>'
+    };
+    server.inject(options, function(res) {
+      code.expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
 
 });
