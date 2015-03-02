1640a1b75ab8298526e6b6e29fce678aa8a4e889
Adam Thody <adam@adamthody.com>
2015-03-02 13:39:49 -0500

    Update route definition format

--- controller/index.js
+++ controller/index.js
@@ -13,7 +13,7 @@ module.exports = yeoman.generators.Base.extend({
   },
   createControllerFile: function() {
     var nameSlug = slug(this.name);
-    this.humanizedSingularName = nameSlug;
+    this.humanizedSingularName = nameSlug.toLowerCase();
     this.template(this.templatePath('controller.js'), this.destinationPath('./controllers/' + nameSlug + 'Controller.js'))
     this.template(this.templatePath('test.js'), this.destinationPath('./test/' + nameSlug + 'Test.js'))
   }
--- controller/templates/controller.js
+++ controller/templates/controller.js
@@ -1,35 +1,13 @@
 'use strict';
 
-// Define handlers
-var controller = {
+exports.routes = [];
 
-  create: function (request, reply) {
-    reply('New controller <%= humanizedSingularName %>').code(201);
-  },
-
-  read: function (request, reply) {
-    reply('New controller <%= humanizedSingularName %>');
-  },
-
-  update: function (request, reply) {
-    reply('New controller <%= humanizedSingularName %>');
-  },
-
-  destroy: function (request, reply) {
-    reply('New controller <%= humanizedSingularName %>');
-  },
-  
-  list: function (request, reply) {
-    reply('New controller <%= humanizedSingularName %>');
+exports.routes.push({
+  method: 'GET',
+  path: '/<%= humanizedSingularName %>',
+  config: {
+    handler: function (request, reply) {
+      reply('Hello world');
+    }
   }
-};
-
-
-// Export routes
-module.exports = [
-  {method: 'POST',    path: '/<%= humanizedSingularName %>', config: {handler: controller.create}},
-  {method: 'GET',     path: '/<%= humanizedSingularName %>/{<%= humanizedSingularName %>?}', config: {handler: controller.read}},
-  {method: 'PUT',     path: '/<%= humanizedSingularName %>', config: {handler: controller.update}},
-  {method: 'DELETE',  path: '/<%= humanizedSingularName %>', config: {handler: controller.destroy}},
-  {method: 'GET',     path: '/<%= humanizedSingularName %>s', config: {handler: controller.list}}
-];
+});
