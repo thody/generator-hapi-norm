5ca3a458f5d50529cffd4b2ff3ec6d04d76b9baf
Adam Thody <adam@adamthody.com>
2015-06-09 14:42:42 -0400

    Add tests

---
+++ app/templates/test/index.js
@@ -0,0 +1,78 @@
+// Load modules
+
+var Hapi = require('hapi');
+var Code = require('code');
+var Lab = require('lab');
+var Server = require('../lib');
+var Version = require('../lib/version');
+var Path = require('path');
+
+//declare internals
+
+var internals = {};
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var it = lab.test;
+
+
+it('starts server and returns hapi server object', function (done) {
+
+  Server.init({}, {}, function (err, server) {
+
+    expect(err).to.not.exist();
+    expect(server).to.be.instanceof(Hapi.Server);
+
+    server.stop(done);
+  });
+});
+
+it('starts server on provided port', function (done) {
+
+  Server.init({connections: [{port: 5000}]}, {}, function (err, server) {
+
+    expect(err).to.not.exist();
+    expect(server.info.port).to.equal(5000);
+
+    server.stop(done);
+  });
+});
+
+it('handles register plugin errors', { parallel: false }, function (done) {
+
+  var orig = Version.register;
+  Version.register = function (server, options, next) {
+
+    Version.register = orig;
+    return next(new Error('register version failed'));
+  };
+
+  Version.register.attributes = {
+    name: 'fake version'
+  };
+
+  Server.init(internals.manifest, internals.composeOptions, function (err, server) {
+
+    expect(err).to.exist();
+    expect(err.message).to.equal('register version failed');
+
+    done();
+  });
+});
+
+internals.manifest = {
+  connections: [
+    {
+      port: 0
+    }
+  ],
+  plugins: {
+    './version': {}
+  }
+};
+
+internals.composeOptions = {
+  relativeTo: Path.resolve(__dirname, '../lib')
+};
---
+++ app/templates/test/version.js
@@ -0,0 +1,54 @@
+// Load modules
+
+var Code = require('code');
+var Lab = require('lab');
+var Pkg = require('../package.json');
+var University = require('../lib');
+var Path = require('path');
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
+var expect = Code.expect;
+var it = lab.test;
+
+
+describe('/version', function () {
+
+  it('returns the version from package.json', function (done) {
+
+    University.init(internals.manifest, internals.composeOptions, function (err, server) {
+
+      expect(err).to.not.exist();
+
+      server.inject('/version', function (res) {
+
+        expect(res.statusCode).to.equal(200);
+        expect(res.result).to.deep.equal({ version: Pkg.version });
+
+        server.stop(done);
+      });
+    });
+  });
+});
+
+internals.manifest = {
+  connections: [
+    {
+      port: 0
+    }
+  ],
+  plugins: {
+    './version': {}
+  }
+};
+
+internals.composeOptions = {
+  relativeTo: Path.resolve(__dirname, '../lib')
+};
